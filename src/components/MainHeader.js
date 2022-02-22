a029f5fb8795c307d72ec0c31200bf67c1b2ad0e
ottl-seo <dotsi@naver.com>
2022-02-22 19:42:47 +0900

    [#12] main페이지 메뉴 링크 추가

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -4,9 +4,9 @@ import styled from "styled-components";
 const Header = () => { 
     return (
     <div className="main_header">
-            <a href="" className="main_menu">Site Map</a>
-            <a href="" className="main_menu">CoC</a>
-            <a href="" className="main_menu">About us</a>
+            <a href="https://www.notion.so/gdsc-ewha/GDSC-Ewha-e256034f486a46f6bc5c05eda1c482cf" className="main_menu">Site Map</a>
+            <a href="https://www.instagram.com/p/CUQB9_PhxGQ/" className="main_menu">CoC</a>
+            <a href="https://gdsc.community.dev/ewha-womans-university/" className="main_menu">About us</a>
     </div>
     )
 }
